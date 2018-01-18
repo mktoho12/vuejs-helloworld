59285c5dac5bc7fd6663086c38d6ba0182e55b07
えむけー <makoto_16n1200102@nnn.ed.jp>
2018-01-19 01:46:13 +0900

    ユーザー入力の制御

--- index.js
+++ index.js
@@ -32,3 +32,23 @@ const app4 = new Vue({
   }
 })
 
+const app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage() {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+const app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+
+
